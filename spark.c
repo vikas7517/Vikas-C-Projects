c69644673eb9785d277bde885e6ed2980021ac17
vikas7517 <121081779+vikas7517@users.noreply.github.com>
2022-12-20 12:05:37 +0530

    Update Spark.c

--- spark.c
+++ spark.c
@@ -2,4 +2,14 @@
 main()
 {
 printf("\n WELCOME");
+printf("\n Spark Institute");
+  add();
+}
+void add()
+{
+int a,b,c;
+  printf("\n entrer a and b=");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  prinf("\n addition=%d",c);
 }
